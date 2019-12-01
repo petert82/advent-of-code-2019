2cd6fee2a4122da9ec0ffdac3806435743f01f20
Peter Thompson <peter.thompson@dunelm.org.uk>
2019-12-01 20:10:57 +0100

    Add Day 1 Part 2 solution

--- src/day1.rs
+++ src/day1.rs
@@ -26,12 +26,49 @@ fn fuel_for_modules(masses: &str) -> Result<Vec<i64>, Error> {
         .map(|line| line.parse::<i64>())
         .collect::<Result<Vec<_>, _>>()?
         .into_iter()
-        .map(|mass| fuel_for_module(mass))
+        .map(|mass| fuel_for_mass(mass))
         .collect())
 }
 
-fn fuel_for_module(mass: i64) -> i64 {
-    (mass / 3) - 2
+fn fuel_for_mass(mass: i64) -> i64 {
+    let fuel = (mass / 3) - 2;
+    if fuel < 0 {
+        return 0;
+    }
+    fuel
+}
+
+pub fn part2(input: &str) {
+    match total_fuel_for_modules_recursive(input) {
+        Ok(total) => println!("The sum of the fuel requirements is: {}", total),
+        Err(Error::BadNumber) => println!("One of the input masses could not be parsed"),
+    }
+}
+
+fn total_fuel_for_modules_recursive(masses: &str) -> Result<i64, Error> {
+    Ok(fuel_for_modules_recursive(masses)?.into_iter().sum())
+}
+
+fn fuel_for_modules_recursive(masses: &str) -> Result<Vec<i64>, Error> {
+    Ok(masses
+        .lines()
+        .map(|line| line.parse::<i64>())
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .map(|mass| fuel_for_module_recursive(mass))
+        .collect())
+}
+
+fn fuel_for_module_recursive(module_mass: i64) -> i64 {
+    let mut tot = 0;
+    let mut fuel = fuel_for_mass(module_mass);
+
+    while fuel > 0 {
+        tot = tot + fuel;
+        fuel = fuel_for_mass(fuel);
+    }
+
+    tot
 }
 
 #[cfg(test)]
@@ -39,11 +76,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_fuel_for_module() {
-        assert_eq!(fuel_for_module(12), 2);
-        assert_eq!(fuel_for_module(14), 2);
-        assert_eq!(fuel_for_module(1969), 654);
-        assert_eq!(fuel_for_module(100756), 33583);
+    fn test_fuel_for_mass() {
+        assert_eq!(fuel_for_mass(12), 2);
+        assert_eq!(fuel_for_mass(14), 2);
+        assert_eq!(fuel_for_mass(1969), 654);
+        assert_eq!(fuel_for_mass(100756), 33583);
     }
 
     #[test]
@@ -51,5 +88,18 @@ mod tests {
         assert_eq!(total_fuel_for_modules("12"), Ok(2));
         assert_eq!(total_fuel_for_modules("12\n14"), Ok(4));
         assert_eq!(total_fuel_for_modules("12\n14\n1969"), Ok(658));
+    }
+
+    #[test]
+    fn test_fuel_for_module_recursive() {
+        assert_eq!(fuel_for_module_recursive(14), 2);
+        assert_eq!(fuel_for_module_recursive(1969), 966);
+        assert_eq!(fuel_for_module_recursive(100756), 50346);
+    }
+
+    #[test]
+    fn test_total_fuel_for_modules_recursive() {
+        assert_eq!(total_fuel_for_modules_recursive("14"), Ok(2));
+        assert_eq!(total_fuel_for_modules_recursive("14\n1969"), Ok(968));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,12 @@ fn main() {
         .author("Peter Thompson <peter.thompson@dunelm.org.uk>")
         .about("Advent of Code 2019 solutions")
         .subcommand(App::new("day1-1").about("Day 1: Part 1"))
+        .subcommand(App::new("day1-2").about("Day 1: Part 2"))
         .get_matches();
 
     match matches.subcommand_name() {
         Some("day1-1") => day1::part1(input::get_input("day1").as_ref()),
+        Some("day1-2") => day1::part2(input::get_input("day1").as_ref()),
         None => println!("You need to specify a day to get a solution"),
         _ => println!("I don't understand :("),
     }
